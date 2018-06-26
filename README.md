04255ab9c35c26e2ffd35ac9b71dbbd20a288c23
Eric Gang <gz120748@gmail.com>
2018-06-26 12:47:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# intellisense-node
+
+This is the README for a vscode extension "intellisense-node".
+
+## Features
+
+Did you ever have the moment when you wanted to accomplish a task in your hand while didn't know which method or even library to use? (npm-intellisense may not work because you probably haven't npm-installed target dependency yet) Describe your task and let this extension find the right API for you.
+
+Currently it only supports a select set of nodejs APIs.
+
+## How this works
+
+TBD
---
+++ src/extension.ts
@@ -0,0 +1,38 @@
+'use strict';
+import * as vscode from 'vscode';
+import fetch from 'node-fetch';
+
+/*
+ * http://esprima.readthedocs.io/en/4.0/getting-started.html#using-node-js-to-play-with-esprima
+ * Refer to the above documentation for AST object structure
+ */
+import { parseScript } from 'esprima';
+
+// this method is called when the extension is activated
+// extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    console.log('intellisense-node is activated');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    const disposable = vscode.commands.registerCommand('extension.searchAPI', () => {
+        vscode.window.showInputBox({prompt: 'Describe the task you want to accomplish'}).then(desc => {
+            const editor = vscode.window.activeTextEditor;       
+            if (editor !== undefined && desc !== undefined) {
+                const position = editor.selection.active;
+                const text = editor.document.getText();
+                fetch("http://localhost:8080/api/search", { 
+                    method: 'POST',
+                    body: `{'desc': ${desc}, 'ast': ${JSON.stringify(parseScript(text))}, 'currentLine': ${position.line}}` 
+                })
+                .then(res => res.json())
+                .then(object => vscode.window.showInformationMessage(object.methodName))
+                .catch(error => console.error('Error: ', error));
+            } 
+        });       
+    });
+
+    context.subscriptions.push(disposable);
+}
